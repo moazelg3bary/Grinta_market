99327d54733a3212b4fe5a98dbffca515976ac14
moaz_home <moazHome@gmail.com>
2020-04-18 22:51:40 +0200

    fixed navbar

--- file.js/app.js
+++ file.js/app.js
@@ -1,4 +1,11 @@
 /*global console, alert, swal, prompt, $*/
-window.onload = function () {
+$(document).ready(function () {
   'use strict';
-}
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() > 100) {
+      $('.navbar').addClass('fixed-top shadow-lg')
+    } else {
+      $('.navbar').removeClass('fixed-top shadow-lg')
+    }
+  })
+})
