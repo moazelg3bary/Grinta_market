6555d355e8a02a3eaccf80d117e911c62909451b
moaz_home <moazHome@gmail.com>
2020-05-05 18:57:36 +0200

    func, change iframe attr src

--- file.js/app.js
+++ file.js/app.js
@@ -1,11 +1,17 @@
 /*global console, alert, swal, prompt, $*/
 $(document).ready(function () {
   'use strict';
+  // if scrollTop navbar is fixed
   $(window).on('scroll', function () {
     if ($(window).scrollTop() > 100) {
       $('.navbar').addClass('fixed-top shadow-lg')
     } else {
       $('.navbar').removeClass('fixed-top shadow-lg')
     }
+  })
+
+  // When click in this, the main_iframe change attr_src 
+  $('.box_video').on('click', function() {
+    $('#main_iframe').attr('src', $(this).find('li:first-of-type .embed-responsive .child_iframe').attr('src'))
   })
 })
